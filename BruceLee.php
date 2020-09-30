0f90ce94f7aa05b897a282cb0110e38a0b02f457
KevinVanphavong <kevin45van@hotmail.fr>
2020-09-30 12:26:56 +0200

    creation sidebar

--- BruceLee.php
+++ BruceLee.php
@@ -17,21 +17,45 @@
             </div>
         </header>
 
-        <div>
+        <div class="sidebar">
 
             <!-- Contient la Description-->
             <section>
 
             </section>
+            <!-- Contient la quick informations-->
+            <section id="firstInformations">
+                <p><b>Basics informations</b></p>
+                    <ul>
+                        <li>Age : 32 years, older but faster than you</li>
+                        <li>🏡 : 'Wherever the wind brings me' </li>
+                        <li>📞 : 911 in  case you need help for fighting</li>
+                        <li>📧 : Check the form down below</li>
+                    </ul>
+            </section>
 
             <!-- Contient les skills -->
-            <section>
-
+            <section id="skillset">
+                <p><b>Skillset</b></p>
+                <ul>
+                    <li>Martial Arts (inventor & precursor of Jeet Kune Do)</li>
+                    <li>Trainer / Teacher</li>
+                    <li>Acting</li>
+                    <li>Producing / Directing movies</li>
+                    <li>Fight choregrapher</li>
+                    <li>Philosopher / Quoter</li>
+                </ul>
             </section>
 
             <!-- Contient les hobbies -->
-            <section>
-
+            <section id="hobbies">
+                <p><b>Hobbies</b></p>
+                    <ul>
+                        <li>Practicing & perfectionning my martial art</li>
+                        <li>Training hard 24/7 to reach the top</li>
+                        <li>Playing ping-pong with nunchaku <a href="https://www.youtube.com/watch?v=qHe6vhexm6g">(Check this out mate)<a/></li>
+                        <li>Doing push-ups with 2 fingers <a href="https://www.youtube.com/watch?v=JlQfBg3VJJU">(Check this out buddy)<a/></li>
+                    </ul>
             </section>
 
         </div>
