b338c33fe5134d4fed980956958887b4a0faddec
Gambino-stack <69907371+Gambino-stack@users.noreply.github.com>
2020-09-30 11:16:41 +0200

    add a description

--- BruceLee.php
+++ BruceLee.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width initial-scale=1.0" />
-        <title>Bruce Lee</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width initial-scale=1.0" />
+    <title>Bruce Lee</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 
-    <body>
-    <section>
-        <h2>DESCRIPTION</h2>
-        <article>Je suis Bruce lee mais ce n'est que mon nom de scene ,  un artiste martial, acteur, réalisateur, producteur et scénariste sino-américain
-            et je suis a la recherche
-        </article>
-    </section>
+<body>
+<section>
+    <h2>DESCRIPTION</h2>
+    <article>
+        "Je sais ce que vous vous dites! Ah c'est Bruce lee ! , mais ce n'est que mon nom de scene, en réalité je m'appelle Lee Jun-fan, moins clinquant non ?
+        Je suis un artiste martial, acteur, réalisateur, producteur et scénariste sino-américain qui trouve encore le temps de faire autre chose de ma vie. En effet,
+        j'ai crée mon propre art martial. Vous voulez fracasser des gueules à la Bruce Lee ? Lancez vous dans l'aventure "jeet kune do" en remplissant le formulaire
+        en bas de la page."
+    </article>
+</section>
 
 
-    </body>
+</body>
 </html>
