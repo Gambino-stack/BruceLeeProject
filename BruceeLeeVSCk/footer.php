f01da9e508fda7f7ac21be55e428179c479099e0
Gambino-stack <69907371+Gambino-stack@users.noreply.github.com>
2020-10-08 15:46:24 +0200

    fix some things

--- BruceeLeeVSCk/footer.php
+++ BruceeLeeVSCk/footer.php
@@ -15,10 +15,10 @@
         <div id="followus">
             <p id="invitation">VIENS ME RETROUVER AVEC MES DISCIPLES SUR LES RÉSEAUX :</p>
             <div id="socialnetwork">
-                <img class="iconsize" src="img/twitter.png" alt="twitter icon" title="twitter" />
-                <img class="iconsize" src="img/facebook.png" alt="facebook icon" title="facebook" />
-                <img class="iconsize" src="img/insta.png" alt="instagram icon" title="instagram" />
-                <img class="iconsize" src="img/snapchat.png" alt="snapchat icon" title="snapchat" />
+               <a class="link" href="https://twitter.com/brucelee?lang=fr"><img class="iconsize" src="img/twitter.png" alt="twitter icon" title="twitter" /></a>
+                <a class="link" href="https://www.facebook.com/BruceLee"><img class="iconsize" src="img/facebook.png" alt="facebook icon" title="facebook" /></a>
+                <a class="link" href="https://www.instagram.com/brucelee/?hl=fr"><img class="iconsize" src="img/insta.png" alt="instagram icon" title="instagram" /></a>
+                <a class="link" href="https://www.pinterest.fr/logistiquevirmousil/bruce-lee/"><img class="iconsize" src="img/snapchat.png" alt="snapchat icon" title="snapchat" /></a>
             </div>
         </div>
         <div id="quotes">
