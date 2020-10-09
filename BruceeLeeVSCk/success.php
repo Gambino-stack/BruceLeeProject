d524d98bb04c63122f24cb672a30aca16d0a0c0e
cedric-n <cedric.ngondo@gmail.com>
2020-10-09 10:24:14 +0200

    style in css the success file

--- BruceeLeeVSCk/success.php
+++ BruceeLeeVSCk/success.php
@@ -39,28 +39,31 @@
         }
 
 
-        if (count($errors) == 0 && $errorEmail != null) { ?>
+        if (count($errors) == 0) { ?>
 
             <div class="message">
                 <div>
                     <h2><?php echo 'Bonjour, voici le récapitulatif des informations que vous nous avez envoyé :'; ?></h2>
-                    <ul>
-                        <li><?php echo 'Votre nom :' . "\t" . htmlentities($dataforms['lastname']); ?></li>
-                        <li><?php echo 'Votre prénom :' . "\t" . htmlentities($dataforms['firstname']); ?></li>
-                        <li><?php echo 'L\'adresse que vous fourni :' . "\t" . htmlentities($dataforms['address']); ?></li>
-                        <li><?php echo 'Votre adresse mail :' . "\t" . htmlentities($dataforms['mail']); ?></li>
-                        <li><?php echo 'Votre age :' . "\t" . htmlentities($dataforms['age']); ?></li>
-                        <li><?php echo 'Votre genre :' . "\t" . htmlentities($dataforms['genre']); ?></li>
-                        <li><?php echo 'L\'audit choisi :' . "\t" . htmlentities($dataforms['audit']); ?></li>
-                        <li><?php echo 'l\'art martial que vous pratiquez:' . "\t" . htmlentities($dataforms['martialArt']); ?></li>
-                        <li><?php echo 'Le message que vous nous avez adressez :' . "\t" . htmlentities($dataforms['message']); ?></li>
-                    </ul>
-                    <p>
-                        <?php echo 'Nous vous répondrons au plus vite à votre message'; ?>
-                    </p>
+                    <div class="information">
+                        <ul>
+                            <li><?php echo 'Votre nom :' . "\t" . htmlentities($dataforms['lastname']); ?></li>
+                            <li><?php echo 'Votre prénom :' . "\t" . htmlentities($dataforms['firstname']); ?></li>
+                            <li><?php echo 'L\'adresse que vous fourni :' . "\t" . htmlentities($dataforms['address']); ?></li>
+                            <li><?php echo 'Votre adresse mail :' . "\t" . htmlentities($dataforms['mail']); ?></li>
+                            <li><?php echo 'Votre age :' . "\t" . htmlentities($dataforms['age']) . ' ans'; ?></li>
+                            <li><?php echo 'Votre genre :' . "\t" . htmlentities($dataforms['genre']); ?></li>
+                            <li><?php echo 'L\'audit choisi :' . "\t" . htmlentities($dataforms['audit']); ?></li>
+                            <li><?php echo 'l\'art martial que vous pratiquez:' . "\t" . htmlentities($dataforms['martialArt']); ?></li>
+                            <li><?php echo 'Le message que vous nous avez adressez :' . "\t" . htmlentities($dataforms['message']); ?></li>
+                        </ul>
+                        <p id="answer">
+                            <?php echo 'Nous vous répondrons au plus vite à votre message'; ?>
+                        </p>
+                    </div>
+
                 </div>
 
-                <div>
+                <div class="img_Success">
                     <img src="img/brucelee-success.png" alt="success sending image" title="I'll call you later">
                 </div>
             </div>
@@ -71,15 +74,15 @@
                 <?php foreach ($errors as $field => $message) { ?>
 
                 <ul class="error">
-                    <li><?php echo htmlentities($message); ?></li>
+                    <li ><?php echo htmlentities($message); ?></li>
                         <?php }
 
                     if ($errorEmail != null ) { ?>
-                        <li><?php echo htmlentities($errorEmail); ?></li>
+                        <li class="normal_error"><?php echo htmlentities($errorEmail); ?></li>
                     <?php }
 
                         if ($errorLengthFirst != null) { ?>
-                            <li><?php echo htmlentities($errorLengthLast); ?></li>
+                            <li class="normal_error"><?php echo htmlentities($errorLengthLast); ?></li>
                        <?php } ?>
 
                 </ul>
